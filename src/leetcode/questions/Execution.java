37eede24e07b79a52a151dcfc55edd9985994690
Umer Wajih <umer3wajih@gmail.com>
2024-05-10 23:04:22 +0500

    Problem 29: Merge k Sorted Lists

--- src/leetcode/questions/Execution.java
+++ src/leetcode/questions/Execution.java
@@ -1,9 +1,16 @@
 package leetcode.questions;
 
+import leetcode.questions.Common.ListNode;
+
 public class Execution {
 
 	public static void main(String[] args) {
-		System.out.print(Problem_11.maxArea(new int[] {1,8,6,2,5,4,8,3,7}));
+		ListNode l1 = new ListNode(1 ,  new ListNode(4, new ListNode(5,null) ) );
+		ListNode l2 = new ListNode(1 ,  new ListNode(3, new ListNode(4,null) ) );
+		ListNode l3 = new ListNode(2 ,  new ListNode(6, null ) );
+		
+		ListNode list = Problem_29.mergeKLists(new ListNode [] {l1,l2,l3});
+		System.out.println(list.val);
 	}
 
 }
---
+++ src/leetcode/questions/Problem_29.java
@@ -0,0 +1,70 @@
+package leetcode.questions;
+
+import leetcode.questions.Common.ListNode;
+
+public class Problem_29 {
+
+	//https://leetcode.com/problems/merge-k-sorted-lists/description/
+	public static ListNode mergeKLists(ListNode[] lists) {
+		return mergeByDividingList(lists, 0, lists.length-1);
+    }
+	
+	//apple merge sort
+	private static ListNode mergeByDividingList(ListNode[] lists, int start, int end) {
+		
+		//when only one list in the array just return that
+		if(start == end) {return lists[start];}
+		
+		//if is start and end are at difference of 1 merge them
+		if(start + 1 == end) {
+			return merge(lists[start], lists[end]);
+		}
+		
+		// separate lists by mid value
+		int mid = start + (end - start) / 2;
+		ListNode left=null, right=null;
+		if(mid != start && mid != end) {
+			left = mergeByDividingList( lists, start, mid);
+			right = mergeByDividingList( lists, mid+1, end);
+		}
+		
+		
+		//once we have 2 lists merge them
+		return merge(left, right);
+	}
+	
+	
+	//merge two lists
+	private static ListNode merge(ListNode node1, ListNode node2) {
+		
+		if(node1 == null) return node2;
+		if(node2 == null) return node1;
+		
+		ListNode returnNode = null, node =null;
+		while (node1 !=null && node2 !=null) {
+			if(node1.val < node2.val) {
+				if(returnNode == null) {
+					returnNode = new ListNode(node1.val);
+					node = returnNode;
+				}else {
+					node.next=new ListNode(node1.val);
+					node = node.next;
+				}
+				node1 = node1.next;
+			}else {
+				if(returnNode == null) {
+					returnNode = new ListNode(node2.val);
+					node = returnNode;
+				}else {
+					node.next=new ListNode(node2.val);
+					node = node.next;
+				}
+				node2 = node2.next;
+			}
+			if(node1==null) { node.next = node2; break;}
+			if(node2==null) { node.next = node1; break;}
+		}
+		return returnNode;
+	}
+	
+}
