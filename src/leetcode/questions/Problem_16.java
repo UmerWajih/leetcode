56d77fb538e64f8354ec153b6322b4a4d0a6fac7
Umer Wajih <umer3wajih@gmail.com>
2024-05-13 21:53:29 +0500

    Problem 15: 3Sum

--- src/leetcode/questions/Problem_16.java
+++ src/leetcode/questions/Problem_16.java
@@ -5,12 +5,6 @@ import java.util.Arrays;
 public class Problem_16 {
 
 	//https://leetcode.com/problems/3sum-closest/
-	
-	//Sort the array
-	//set target to integer max
-	//if sum  < diff update diff
-	// ignore duplicates for time purpose
-	// d k--
 	public static int threeSumClosest(int[] nums, int target) {
         
 		int i=0, j=i+1, k=nums.length-1;
@@ -20,14 +14,8 @@ public class Problem_16 {
 		while (i < j && j < nums.length-1) {
         	while (j < k) {
         		int sum = nums[i] + nums[j] + nums[k];
-        		//find diff between sum and target
             	int abs = Math.abs(sum -  target );
-            	
-            	// if sum and target equal then return sum
             	if(abs==0) {return sum;}
-            	
-            	
-            	
             	if(sum > target) {
             		// to avoid repititons
                 	while(k-1 > j && nums[k] == nums[k-1]) {
@@ -41,13 +29,11 @@ public class Problem_16 {
         			} 
             		j++;
             	}
-            	//if abs value is less than previous diff save this
             	if(abs<= diff) {
             		diff = abs;
     				sumFinal = sum;
             	}
         	}
-        	
         	i++; j=i+1;k=nums.length-1;
         }
 		return sumFinal;
