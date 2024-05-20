e6f50a9a14bcb80f8788307774835ada285de2da
Umer Wajih <umer3wajih@gmail.com>
2024-05-20 22:56:09 +0500

    Problem 48:\ Rotate Image

--- src/leetcode/questions/Execution.java
+++ src/leetcode/questions/Execution.java
@@ -4,7 +4,7 @@ public class Execution {
 
 	public static void main(String[] args) {
 		
-		System.out.println(Problem_32.longestValidParentheses(")()"));
+		Problem_48.rotate(new int [][] {{1,2,3},{4,5,6},{7,8,9}});
 	}
 
 }
---
+++ src/leetcode/questions/Problem_48.java
@@ -0,0 +1,31 @@
+package leetcode.questions;
+
+public class Problem_48 {
+
+	//https://leetcode.com/problems/rotate-image/
+	public static  void rotate(int[][] matrix) {
+        //rotate i with j 
+		int col = matrix.length;
+		int row = matrix[0].length;
+		for (int i=0; i< row; i++) {
+			for (int k=i; k< col; k++) {
+				int t = matrix[i][k];
+				matrix[i][k] = matrix[k][i]; 
+				matrix[k][i] = t;
+			}
+		}
+		
+		
+		// now reverse the colums
+		int start = 0, end=col-1;
+		for(int i=0; i< row; i++) {
+			while(start < end) {
+				int t = matrix[row][end];
+				matrix[row][end] = matrix[row][start];
+				matrix[row][start] = t;
+				start++;end--;				
+			}
+			start = 0; end=col-1;
+		}
+    }
+}
