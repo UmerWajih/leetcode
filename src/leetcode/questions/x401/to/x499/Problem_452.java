d0e4837061aa7232083d36c88951fa12f08227e0
Umer Wajih <umer3wajih@gmail.com>
2024-06-28 08:18:04 +0500

    Problem 452: Minimum Number of Arrows to Burst Balloons

--- src/leetcode/questions/x401/to/x499/Problem_452.java
+++ src/leetcode/questions/x401/to/x499/Problem_452.java
@@ -7,11 +7,6 @@ public class Problem_452 {
 	
 	//https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons
 public int findMinArrowShots(int[][] points) {
-	//[[1,2],[2,3],[3,4],[4,5]]
-	// if the max value is less than 1 
-	
-	
-	//[,[7,12],[3,8],[6,8],[9,10],[2,9],[0,9],[3,9],[0,6],[2,8]]
 	
         Arrays.sort(points, (x1,x2) -> Integer.compare(x1[1] , x2[1]));
         int count =1;
