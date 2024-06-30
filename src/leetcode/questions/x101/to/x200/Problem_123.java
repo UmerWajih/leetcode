370f0e0e701dd7e853e5f30532f67d2f5ce7e143
Umer Wajih <umer3wajih@gmail.com>
2024-07-01 03:06:23 +0500

    Problem 123: Best Time to Buy and Sell Stock III

--- src/leetcode/questions/x101/to/x200/Problem_123.java
+++ src/leetcode/questions/x101/to/x200/Problem_123.java
@@ -33,6 +33,8 @@ public class Problem_123 {
 				start = i;
 				continue;
 			}
+			
+			if(prices[i] < prices[start] && !profitFromIndex.isEmpty())break;
 			if(prices[i] > prices[start]) {
 				profitFromIndex.put(new Integer[] {start, i}, prices[i] - prices[start]);
 			}
@@ -68,9 +70,20 @@ public class Problem_123 {
 	public static int maxProfit(int[] prices) {
 		Map<Integer[], Integer> profit = new HashMap<>();
 		recursive(prices, 0, profit); 
-//		for (Integer[] val : profit.keySet()) {
-//			System.out.println("Key:"+ val[0]+","+val[1]+"  : Value :  "+profit.get(val));
-//		}
+		for (Integer[] val : profit.keySet()) {
+			System.out.println("Key:"+ val[0]+","+val[1]+"  : Value :  "+profit.get(val));
+		}
 		return getMaxProfits(profit);
     }
+	//if you dont want to use DP use simple math solution
+//	public static int maxProfit(int[] prices) {
+//		int sell1 = 0, sell2 = 0, buy1 = Integer.MIN_VALUE, buy2 = Integer.MIN_VALUE;
+//		for (int i = 0; i < prices.length; i++) {
+//			buy1 = Math.max(buy1, -prices[i]);
+//			sell1 = Math.max(sell1, buy1 + prices[i]);
+//			buy2 = Math.max(buy2, sell1 - prices[i]);
+//			sell2 = Math.max(sell2, buy2 + prices[i]);
+//		}
+//		return sell2;
+//    }
 }
