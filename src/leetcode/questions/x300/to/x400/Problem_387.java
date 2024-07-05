2a29524a74744e39d8fb8fad91a139330037590e
Umer Wajih <umer3wajih@gmail.com>
2024-07-06 03:49:01 +0500

    Problem 387: First Unique Character in a String

--- src/leetcode/questions/x300/to/x400/Problem_387.java
+++ src/leetcode/questions/x300/to/x400/Problem_387.java
@@ -1,5 +1,32 @@
 package leetcode.questions.x300.to.x400;
 
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Problem_387 {
 
+	// https://leetcode.com/problems/first-unique-character-in-a-string/
+	public int firstUniqChar(String s) {
+		Set<Character> set = new HashSet<>();
+		Map<Character, Integer> map = new LinkedHashMap<>();
+		char[] arr = s.toCharArray();
+		for (int i = 0; i < arr.length; i++) {
+			if (set.contains(arr[i]))
+				continue;
+			if (map.containsKey(arr[i])) {
+				map.remove(arr[i]);
+				set.add(arr[i]);
+			} else {
+				map.put(arr[i], i);
+			}
+		}
+
+		for (Character c : map.keySet()) {
+			return map.get(c);
+		}
+
+		return -1;
+	}
 }
