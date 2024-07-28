4dd937f5ba795a25fb3eddf841f55befdd5aed66
Umer Wajih <umer3wajih@gmail.com>
2024-07-28 18:23:50 +0500

    Problem 347: Top K Frequent Elements

---
+++ src/leetcode/questions/x300/to/x400/Problem_347.java
@@ -0,0 +1,24 @@
+package leetcode.questions.x300.to.x400;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.PriorityQueue;
+
+public class Problem_347 {
+	//https://leetcode.com/problems/top-k-frequent-elements/
+	public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+		for (int i =0; i< nums.length; i++) {
+			map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+		}
+		int [] output = new int[k];
+		PriorityQueue<Entry<Integer, Integer>> q = new PriorityQueue<>((a,b)-> b.getValue()-a.getValue());
+		q.addAll(map.entrySet());
+		
+		for(int i=0; i<k;i++) {
+			output[i]= q.poll().getValue();
+		}
+		return output;
+    }
+}
