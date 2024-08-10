a24f04bf23a5e202f21990382f96d6ac9ff2f77f
Umer Wajih <umer3wajih@gmail.com>
2024-08-11 04:15:37 +0500

    Problem 1812: Determine Color of a Chessboard Square

--- src/leetcode/questions/x1800/to/x1899/Problem_1812.java
+++ src/leetcode/questions/x1800/to/x1899/Problem_1812.java
@@ -1,5 +1,12 @@
 package leetcode.questions.x1800.to.x1899;
 
 public class Problem_1812 {
-
+	//https://leetcode.com/problems/determine-color-of-a-chessboard-square/
+	public boolean squareIsWhite(String coordinates) {
+        int [][]board = new int[][]{
+            {0,1},{1,0}
+        };
+     if(board[(8 -(coordinates.charAt(1)-'0'))%2][ (coordinates.charAt(0)-'a')%2] == 0) return true;
+        else return false;
+    }
 }
