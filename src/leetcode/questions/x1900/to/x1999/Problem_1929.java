565e12f95618598b262232409df0c5c337829048
Umer Wajih <umer3wajih@gmail.com>
2024-11-13 18:43:42 +0500

    Problem 1929: Concatenation of Array

---
+++ src/leetcode/questions/x1900/to/x1999/Problem_1929.java
@@ -0,0 +1,13 @@
+package leetcode.questions.x1900.to.x1999;
+
+//https://leetcode.com/problems/concatenation-of-array/
+public class Problem_1929 {
+	public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length *2];
+        for(int i=0 ;i <nums.length ; i++ ){
+            ans[i] = nums[i];
+            ans[i+nums.length] = nums[i];
+        }
+        return ans;
+    } 
+}
