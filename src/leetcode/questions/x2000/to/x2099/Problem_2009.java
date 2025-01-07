0444ab9b5cb0fe872ba26b325dce8100d4554d95
Umer Wajih <umer3wajih@gmail.com>
2025-01-07 14:09:08 +0500

    Problem 2009: Minimum Number of Operations to Make Array Continuous

--- src/leetcode/questions/x2000/to/x2099/Problem_2009.java
+++ src/leetcode/questions/x2000/to/x2099/Problem_2009.java
@@ -1,10 +1,8 @@
 package leetcode.questions.x2000.to.x2099;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Deque;
+import java.util.LinkedList;
 
 //https://leetcode.com/problems/minimum-number-of-operations-to-make-array-continuous/
 public class Problem_2009 {
@@ -14,41 +12,21 @@ public class Problem_2009 {
 		//o(nlogN)
 		Arrays.sort(nums);
 		if(nums.length==1) return 0;
-		
-//		List<Integer> duplicates = new ArrayList<>();
-//		Map<Integer, Integer> count = new HashMap<>();
-//		
-//		for(int i:nums) {
-//			count.put(i, count.getOrDefault(i, 0)+1);
-//			if(count.get(i) > 1)duplicates.add(i);
-//		}
-//		if(nums[nums.length-1]-nums[0] == nums.length-1) {
-//			//here last and first value are valid
-//			//only duplicates, so we can just return the size
-//			return duplicates.size();
-//		}
-		
-		int min= Integer.MAX_VALUE;
-		//start from 0, check how many changes will be needed
-		for (int i=0; i<nums.length;i++) {
-			int l=i-1, r=i+1;
-			int lCount=0, rCount=0;
-			int lIndex=1,rIndex=1;
-			while(l>=0) {
-				if(nums[l] != nums[i]-lIndex) {
-					lCount++;
-				}
-				l--; lIndex--;
+		Deque<Integer> window = new LinkedList<>();
+		int max=0;
+		for(int i : nums) {
+			while(!window.isEmpty() && i - window.peekFirst() >= nums.length) {
+				//remove from window
+				window.poll();
 			}
-			while(r < nums.length) {
-				if(nums[r] != nums[i]+rIndex) {
-					rCount++;
-				}
-				r++; rIndex++;
+			
+			if(window.isEmpty() || window.peekLast() != i) {
+				window.offer(i);
 			}
-			min=Math.min(min, rCount+lCount);
+			max= Math.max(max, window.size());
+			
 		}
-		
-		return min;
+		return nums.length-max;
+		//
     }
 }
